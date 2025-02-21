a51a0dd6445de2c299d2781fddc5ed13d82e9311
Leonor-F-G-Almeida <up202405796@up.pt>
2025-02-21 18:11:18 +0000

    First Commit

---
+++ src/Automovel.java
@@ -0,0 +1,82 @@
+public class Automovel {
+    private String matricula;
+    private String marca;
+    private int cilindrada;
+
+    private static final String MATRICULA_POR_OMISSAO = "sem matrícula";
+    private static final String MARCA_POR_OMISSAO = "sem marca";
+    private static final int CILINDRADA_POR_OMISSAO = 0;
+
+    private static int totalAutomoveis = 0;
+
+    public Automovel() { // construtor vazio, por omissao (nao conhecemos nada do objeto)
+        matricula = MATRICULA_POR_OMISSAO;
+        marca = MARCA_POR_OMISSAO;
+        cilindrada = CILINDRADA_POR_OMISSAO;
+        totalAutomoveis++;
+    }
+    public Automovel(String matricula, String marca, int cilindrada) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.cilindrada = cilindrada;
+        totalAutomoveis++; //contador de instancias, sempre que se cria um objeto
+    }
+    public Automovel(String matricula, String marca) {
+        this.matricula = matricula;
+        this.marca = marca;
+        cilindrada = CILINDRADA_POR_OMISSAO;
+        totalAutomoveis++;
+    }
+
+    public Automovel(String marca, int cilindrada) {
+        matricula = MATRICULA_POR_OMISSAO;
+        this.marca = marca;
+        this.cilindrada = cilindrada;
+        totalAutomoveis++;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public String toString() {
+        return "O automóvel com matrícula " + matricula + " é um " + marca + " de " + cilindrada + " cc.";
+    }
+
+    public int calcularDiferencaCilindrada(Automovel outroAutomovel) {
+        return (cilindrada - outroAutomovel.getCilindrada());
+    }
+
+    public boolean isCilindradaMaior(Automovel outroAutomovel) {
+        return cilindrada > outroAutomovel.getCilindrada();
+    }
+
+    public boolean isCilindradaMaior(int cilindrada) {
+        return this.cilindrada > cilindrada;
+    }
+
+    public static int getTotalAutomoveis() {
+        return totalAutomoveis;
+    }
+
+}
